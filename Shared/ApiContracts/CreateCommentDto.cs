a155789561bb98f1538900674d2db3aa62610a6d
Smarties <basale95@gmail.com>
2025-10-07 08:36:17 +0200

    commet + post dtos

---
+++ Shared/ApiContracts/CreateCommentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiContracts;
+
+public class CreateCommentDto
+{
+    public int PostId { get; set; }
+    public string Username { get; set; }
+    public int UserId { get; set; }
+    public string Content { get; set; }
+}
