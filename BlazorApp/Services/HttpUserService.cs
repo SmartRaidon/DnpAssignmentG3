66d215d437bb0f2645685128a2877cd11b364599
Smarties <355510@via.dk>
2025-10-29 18:09:44 +0100

    started assignment 5

---
+++ BlazorApp/Services/HttpUserService.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using ApiContracts;
+
+namespace BlazorApp.Services;
+
+public class HttpUserService : IUserService
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpUserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<UserDto> AddUserAsync(CreateUserDto request)
+    {
+        HttpResponseMessage httpResponse = await _httpClient.PostAsJsonAsync("users", request); // api/users maybe maybe
+        string response = await httpResponse.Content.ReadAsStringAsync();
+        if (!httpResponse.IsSuccessStatusCode)
+        {
+            throw new Exception(response);
+        }
+        return JsonSerializer.Deserialize<UserDto>(response, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+    }
+
+    public Task<UserDto> GetUserAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateUserAsync(int id, UserDto request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteUserAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BlazorApp/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using ApiContracts;
+
+namespace BlazorApp.Services;
+
+public interface IUserService
+{
+    public Task<UserDto> AddUserAsync(CreateUserDto request);
+    public Task<UserDto> GetUserAsync(int id); // id or username im not sure for now
+    public Task UpdateUserAsync(int id, UserDto request);
+    public Task DeleteUserAsync(int id);
+}
