1675cc2a6b9324878d7402a8fb6570777d55510d
Smarties <355510@via.dk>
2025-11-19 22:26:06 +0100

    #8 almost finished, missing: EfcCommentRepository implementation

---
+++ Server/EfcRepositories/EfcCommentRepository.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace EfcRepositories;
+
+public class EfcCommentRepository : ICommentRepository
+{
+    private readonly AppContext _context;
+
+    public EfcCommentRepository(AppContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        throw new NotImplementedException();
+    }
+}
