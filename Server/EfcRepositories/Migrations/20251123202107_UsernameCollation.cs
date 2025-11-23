83f27d2a1b86487c6e8261b033367f5c865328c5
Smarties <355510@via.dk>
2025-11-23 22:12:49 +0100

    added migrations to git

---
+++ Server/EfcRepositories/Migrations/20251123202107_UsernameCollation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfcRepositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsernameCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldCollation: "NOCASE");
+        }
+    }
+}
